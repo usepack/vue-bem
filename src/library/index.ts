941d0c9bdb97586ec56c459a8df34db8895d765c
intelrug <intelrug@gmail.com>
2022-11-16 17:30:33 +0300

    Fix use of custom block name

--- src/library/index.ts
+++ src/library/index.ts
@@ -5,6 +5,11 @@ import { getClassName } from './helpers';
 export function install (Vue: App, { hyphenate }: BemOptions): void {
   Vue.mixin({
     name: 'BemMixin',
+    data () {
+      return {
+        bemMixinBlockName: ''
+      };
+    },
     methods: {
       $bem ({ b, e, m }: BemItem): string[] {
         const pascalToKebabCase = (s: string) => s
@@ -12,8 +17,13 @@ export function install (Vue: App, { hyphenate }: BemOptions): void {
           .replace(/([a-z])([A-Z])/g, '$1-$2')
           .replace(/[\s_]+/g, '-')
           .toLowerCase();
+
+        if (b) {
+          this.bemMixinBlockName = b;
+        }
+
         const n = this.$options.name;
-        const block: string = b || (hyphenate ? pascalToKebabCase(n) : n);
+        const block: string = this.bemMixinBlockName || (hyphenate ? pascalToKebabCase(n) : n);
         const modifiers: string[] = m ? (typeof m === 'string' ? [m] : (Array.isArray(m) ? m : Object.keys(m).filter(key => m[key]))) : [];
         const elementClass = getClassName(block, e);
         const modifiersClasses = modifiers.map(m => getClassName(block, e, m));
